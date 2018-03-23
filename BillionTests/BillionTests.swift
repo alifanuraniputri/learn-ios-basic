f51010662a41cd460612169bb378ff886699c35d
alifanuraniputri <alifanuraniputri@gmail.com>
2018-03-23 08:55:28 +0700

    Initial Commit

---
+++ BillionTests/BillionTests.swift
@@ -0,0 +1,36 @@
+//
+//  BillionTests.swift
+//  BillionTests
+//
+//  Created by Nakama on 23/03/18.
+//  Copyright © 2018 Nakama. All rights reserved.
+//
+
+import XCTest
+@testable import Billion
+
+class BillionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
